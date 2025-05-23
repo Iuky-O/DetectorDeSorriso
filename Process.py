6cb0e535499ca177c4e152a6278bf68f212d87e7
Iuky.O <iumyfarias03@gmail.com>
2025-05-23 19:08:40 -0300

    Enviando tudo

---
+++ Process.py
@@ -0,0 +1,59 @@
+import cv2
+import requests
+
+esp32_ip = 'http://10.0.0.61'
+
+# Carregar o classificador em cascata para detectar rostos e sorrisos
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
+
+cap = cv2.VideoCapture(0) #aqui muda a camera.... 
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Converter imagem para escala de cinza
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detectar rostos na imagem
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    smile_detected = False
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+        
+        # Detectar sorrisos na região de interesse
+        smiles = smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.8, minNeighbors=20, minSize=(25, 25))
+        
+        # Desenhar máscara ao redor da região de interesse
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
+        cv2.putText(frame, "Sorria", (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 255), 2)
+        
+        if len(smiles) > 0:
+            smile_detected = True
+            cv2.putText(frame, "Sorriso detectado", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+        else:
+            cv2.putText(frame, "Nenhum sorriso", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+        break
+    # Enviar comandos para o ESP32
+    if smile_detected:
+        requests.get(f"{esp32_ip}/LED=ON") 
+        print("Sorriso detectado - LED ligado")
+    else:
+        requests.get(f"{esp32_ip}/LED=OFF") 
+        print("Nenhum sorriso detectado - LED desligado")
+
+    # Mostrar imagem
+    cv2.imshow('Frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
